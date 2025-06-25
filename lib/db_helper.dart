507a116c952dcf3f5dd352b3aab53ef4b7216ca5
raunakpurohit <raunak.wscubetech@gmail.com>
2025-06-25 23:20:23 +0530

    add and fetch in UI

--- lib/db_helper.dart
+++ lib/db_helper.dart
@@ -13,6 +13,12 @@ class DbHelper{
 
   Database? mDB;
 
+  static const String TABLE_NOTE = "note";
+  static const String COLUMN_NOTE_ID = "n_id";
+  static const String COLUMN_NOTE_TITLE = "n_title";
+  static const String COLUMN_NOTE_DESC = "n_desc";
+  static const String COLUMN_NOTE_CREATED_AT = "n_created_at";
+
   Future<Database> initDB() async{
 
     mDB ??= await openDB();
@@ -21,7 +27,7 @@ class DbHelper{
 
   }
 
-   Future<Database> openDB() async{
+  Future<Database> openDB() async{
     Directory appDir = await getApplicationDocumentsDirectory();
     String dbPath = join(appDir.path, "notesDB.db");
 
@@ -29,20 +35,35 @@ class DbHelper{
         onCreate: (db, version){
 
       /// all the tables here...
+          db.execute("create table $TABLE_NOTE ( $COLUMN_NOTE_ID integer primary key autoincrement, $COLUMN_NOTE_TITLE text, $COLUMN_NOTE_DESC text, $COLUMN_NOTE_CREATED_AT text)");
 
     });
 
   }
 
   ///queries
-  void addNote() async{
+  Future<bool> addNote({required String title, required String desc}) async{
     var db = await initDB();
+
+    int rowsEffected = await db.insert(TABLE_NOTE, {
+      COLUMN_NOTE_TITLE : title,
+      COLUMN_NOTE_DESC : desc,
+      COLUMN_NOTE_CREATED_AT : DateTime.now().millisecondsSinceEpoch.toString()
+    });
+
+    return rowsEffected>0;
 
   }
 
-  void fetchAllNotes() async{
+  Future<List<Map<String,dynamic>>> fetchAllNotes() async{
     var db = await initDB();
 
+    List<Map<String, dynamic>> allData = await db.query(TABLE_NOTE);
+
+    return allData;
   }
+
+  ///update
+  ///delete
 
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,19 +1,65 @@
 import 'package:db_exp_395/db_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  State<HomePage> createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  DbHelper? dbHelper;
+  List<Map<String, dynamic>> allNotes = [];
+  DateFormat df = DateFormat.yMMMEd();
+
+  @override
+  void initState() {
+    super.initState();
     ///DbHelper dbHelper = DbHelper();
-    DbHelper dbHelper = DbHelper.getInstance();
+    dbHelper = DbHelper.getInstance();
+    getAllNotes();
+  }
 
+  void getAllNotes() async{
+    allNotes = await dbHelper!.fetchAllNotes();
+    setState(() {
+
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
       ),
+      body: allNotes.isNotEmpty ? ListView.builder(
+        itemCount: allNotes.length,
+          itemBuilder: (_, index){
+
+        return Card(
+          child: ListTile(
+            title: Text(allNotes[index][DbHelper.COLUMN_NOTE_TITLE]),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(allNotes[index][DbHelper.COLUMN_NOTE_DESC]),
+                Text(df.format(DateTime.fromMillisecondsSinceEpoch(int.parse(allNotes[index][DbHelper.COLUMN_NOTE_CREATED_AT])))),
+              ],
+            ),
+          ),
+        );
+
+      }) : Center(child: Text("No Notes yet!!"),),
+      floatingActionButton: FloatingActionButton(onPressed: () async{
+        bool check = await dbHelper!.addNote(title: "New Note", desc: "This is Amazing!!");
+        if(check){
+          print("Note Added");
+          getAllNotes();
+        }
+      }, child: Icon(Icons.add),),
     );
   }
 }
